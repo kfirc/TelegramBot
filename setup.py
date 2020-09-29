167682a826c1567e59ef68d342e31c8383f51c4a
Kfir Cohen <kfir969@agmail.com>
2020-09-29 23:17:34 +0300

    Add package installation support with setuptools

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TelegramBot',
+    version='1',
+    description='A tool to control and maintain telegram bots',
+    url='https://github.com/kfirc/TelegramBot',
+    author='Kfir Cohen',
+    author_email='kfir969@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+    ],
+)
